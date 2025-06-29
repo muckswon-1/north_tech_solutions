ac1c89e644a24bec809417547d9f3c647c361b09
Mucks Won <mukabwacyril@gmail.com>
2025-06-29 06:44:15 -0500

    env file fix

--- frontend/src/api/firebase.js
+++ frontend/src/api/firebase.js
@@ -8,13 +8,7 @@ import { getAuth, RecaptchaVerifier } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBFwWYQ8SOPtlAb_sL4kF5l9zlxBpA9Fb8",
-  authDomain: "sokoni-464a6.firebaseapp.com",
-  projectId: "sokoni-464a6",
-  storageBucket: "sokoni-464a6.firebasestorage.app",
-  messagingSenderId: "548440026088",
-  appId: "1:548440026088:web:9223dbb0cd358a141fd25d",
-  measurementId: "G-B7JT4255PB"
+  
 };
 
 // Initialize Firebase
